730ec098694c1c781d02bf5bd49598b4f4e6c10c
mishiyama <ayushlamsal931@gmail.com>
2025-09-21 13:00:56 +0545

    :)

--- Linear Regression/lr-model2.py
+++ Linear Regression/lr-model2.py
@@ -14,6 +14,54 @@
 #     name: python3
 # ---
 
-##########IMPLEMENTAION OF MULTIPLE LINEAR REGRESSION##########
+# #########IMPLEMENTAION OF MULTIPLE LINEAR REGRESSION##########
 
 
+# Importing required dependencies
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+
+#To read the data from CSV file
+df=pd.read_csv('realestate_data.csv')
+df.drop(columns=['House ID'])
+
+#Assign indepe ndent and dependent features
+X =df.iloc[:,:-1] 
+y=df['Price (USD)']
+
+#Test-Train split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=42)
+
+
+#Standardization 
+scaler=StandardScaler()                    # z-score normalization (useful when features in data set have different units or scales) 
+X_train=scaler.fit_transform(X_train)
+X_test=scaler.transform(X_test)            # using transform in test data to prevent data leakage (no info regarding train data to be revealed to test)
+
+#Applying Simple linear regression 
+regression=LinearRegression()
+regression.fit(X_train,y_train)
+print("Coefficent: ",regression.coef_)
+print("Intercept: ",regression.intercept_)
+
+
+y_pred_train = regression.predict(X_train)
+y_pred_test = regression.predict(X_test)
+
+print("Train R² Score:", r2_score(y_train, y_pred_train))
+print("Test R² Score:", r2_score(y_test, y_pred_test))
+print("MAE:", mean_absolute_error(y_test, y_pred_test))
+print("MSE:", mean_squared_error(y_test, y_pred_test))
+plt.scatter(y_test, y_pred_test)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+plt.title("Actual vs Predicted Prices")
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--')  # Diagonal line
+plt.show()
